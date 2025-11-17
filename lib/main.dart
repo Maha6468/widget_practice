ff22c519f7bfba49db9a3a8f5de5774207889188
Maha6468 <motherma7633@gmail.com>
2025-11-17 16:23:59 +0600

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:ListView_All()
+      home:TodoApp()
     );
   }
 }
